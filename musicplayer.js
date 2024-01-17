2c56585e2bd8eb1383514e657788c0e6e9331fad
Anthony-Mbaya <120335962+Anthony-mbaya@users.noreply.github.com>
2024-01-17 19:44:11 +0300

    modified files

--- musicplayer.js
+++ musicplayer.js
@@ -3,51 +3,159 @@ const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
 const nextButton = document.getElementById("next");
 const shuffleButton = document.getElementById("shuffle");
+const playListSongs = document.getElementById("playlist-songs");
 
 const mySongs = [
     {
-        id:0,
-        title:"world titch",
+        no:0,
+        title:"cant",
         artist:"costa titch",
         duration:"5:00",
-        source:"music-player/@costatitchworld_-_Big_Flexa_ft._C'buda_M,_Alfa_Kat,_Banaba_Des,_Sdida_&_Man_T_(Official_Music_Video)(128k).m4a"
+        source:"https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3"
     
     },
     {
-        id:1 ,
-        title:"shape of you ",
+        no:1 ,
+        title:"learning ",
         artist:"ed sheeran ",
         duration:"5:00 ",
-        source:"music-player/Ed_Sheeran_-_Shape_Of_You_(Lyrics)(128k).m4a "
+        source:"https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3 "
     },
     {
-        id:2 ,
+        no:2 ,
         title:"cough ",
         artist:"kizz daniel ",
         duration:"6:00 ",
-        source:"music-player/Kizz_Daniel,_EMPIRE_-_Cough_(Official_Video)(128k).m4a "
+        source:"https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3 "
     },
     {
-        id:3 ,
+        no:3 ,
         title:"mama amina ",
         artist:"marioo ft madjoz ",
         duration:"5:00 ",
-        source:"music-player/Marioo_ft_Sho_Madjozi_&_Bontle_Smith_-_Mama_Amina_(Official_Video)(128k).m4a "
+        source:"https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3 "
     } 
 ]
-//create a new HTMLAudioElement 
+
+//createS a new HTMLAudioElement 
+//its a WEB AUDIO API
 const audio = new Audio();
 
 //new object to alternate
 const alterData = {
-    songs:[...mySongs],
-    songCurrentTime : 0,
-    currentSong:null
+    songs:[...mySongs],//copy of all songs
+    currentSong:null,
+    songCurrentTime : 0
+    
 };
 
-//function to play song
-//find method returns the value of the first element that passes a test
-const playSongFunc = () =>{
-
+//Optional chaining (?.) helps prevent errors when accessing nested properties that might be null or undefined. 
+const dispSongs =(arr)=>{
+    //creates a new array after performing a function for every array element. 
+   const songToHtml = arr.map((theSong) => {
+        return `
+        <li id="${theSong.no}">
+        <button onclick="playSongFunc(${theSong.no})"> 
+             <span class="display">${theSong.title}</span> 
+             <span class="display">${theSong.artist}</span>
+               <span class="display">${theSong.duration}</span>
+        </button> 
+        `;
+        // used to concatenate all the elements of an array into a single string
+    }).join("");
+    playListSongs.innerHTML = songToHtml;
 }
 
+dispSongs(alterData?.songs);
+
+//function to play song
+const playSongFunc = (no) =>{
+    //find method returns the value of the first element that passes the test
+    const findSong = alterData?.songs.find((fSong) => fSong.no === no);
+
+    audio.src = findSong.source;
+    audio.title = findSong.title;
+
+    //if state to check if there is song playing
+    if(alterData?.currentSong === null || alterData.currentSong.x !== findSong.no){
+        audio.currentTime = 0;
+    }else{
+        audio.currentTime = alterData.songCurrentTime;
+    }
+    alterData.currentSong = findSong;
+
+    setPlayerDisplay();
+
+    audio.play();
+}
+
+ 
+playButton.addEventListener("click",() =>{
+    if(alterData?.currentSong === null){
+        playSongFunc(alterData?.songs[0].no);
+    }else{
+        playSongFunc(alterData?.currentSong.no);
+    }
+});
+
+const pauseSongFunc =() =>{
+     alterData.songCurrentTime = audio.currentTime;
+     audio.pause();
+}
+
+pauseButton.addEventListener("click",pauseSongFunc);
+
+
+
+const nextSongFunc =()=>{
+    if(alterData?.currentSong === null){
+        playSongFunc(alterData?.songs[0].no);
+    }else{
+        const currentSongIndex = getPlayingSongIndex();
+        const nextSong = alterData?.songs[currentSongIndex + 1];
+
+        playSongFunc(nextSong.no);
+    }
+};
+
+nextButton.addEventListener("click",nextSongFunc);
+
+//indexOf returns the first index at which a given element can be found in the array,
+const getPlayingSongIndex = () => alterData?.songs.indexOf(alterData.currentSong);
+
+const prevSongFunc =()=>{
+    if(alterData?.currentSong === null){
+        return;
+    }else{
+        const currentSongIndex = getPlayingSongIndex();
+        const prevSong = alterData?.songs[currentSongIndex - 1];
+
+        playSongFunc(prevSong.no);
+    }
+};
+
+prevButton.addEventListener("click",prevSongFunc);
+
+const shuffleFunc =()=>{
+    alterData?.songs.sort(() => Math.random() - 0.5);
+    alterData?.currentSong = null;
+    alterData?.songCurrentTime = 0;
+
+    dispSongs(alterData?.songs);
+    playSongFunc();
+    setPlayerDisplay();
+}
+
+shuffleButton.addEventListener("click",shuffleFunc);
+
+//want to display current playing song
+const setPlayerDisplay =()=>{
+    const playingsongTitle = document.getElementById("sTitle");
+    const playingsongArtist = document.getElementById("sArtist");
+    
+    const currentTitle = alterData?.currentSong?.title;
+    const currentArtist = alterData?.currentSong?.artist;
+
+    playingsongTitle.textContent = currentTitle ? currentTitle: "";
+    playingsongArtist.textContent = currentArtist ? currentArtist:"";
+}
